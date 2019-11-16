91c86b82e0977ebd0e4b1e02352966dd91af7816
YanlinWang <chinese1student@gmail.com>
2019-11-16 14:56:18 +0800

    客户端分离抽象出socket类

--- client.cpp
+++ client.cpp
@@ -1,122 +1,7 @@
-#define WIN32_LEAN_AND_MEAN
-
-#include<windows.h>
-#include<WinSock2.h>
-#include<stdio.h>
+#include "EasyTcpClient.h"
 #include<thread>
 
-#pragma comment(lib,"ws2_32.lib")
-
-enum CMD
-{
-	CMD_LOGIN,
-	CMD_LOGIN_RESULT,
-	CMD_LOGOUT,
-	CMD_LOGOUT_RESULT,
-	CMD_NEW_USER_JOIN,
-	CMD_ERROR
-};
-
-struct DataHeader
-{
-	short dataLength;
-	short cmd;
-};
-
-//DataPackage
-struct Login : public DataHeader
-{
-	Login()
-	{
-		dataLength = sizeof(Login);
-		cmd = CMD_LOGIN;
-	}
-	char userName[32];
-	char PassWord[32];
-};
-
-struct LoginResult : public DataHeader
-{
-	LoginResult()
-	{
-		dataLength = sizeof(LoginResult);
-		cmd = CMD_LOGIN_RESULT;
-		result = 0;
-	}
-	int result;
-};
-
-struct Logout : public DataHeader
-{
-	Logout()
-	{
-		dataLength = sizeof(Logout);
-		cmd = CMD_LOGOUT;
-	}
-	char userName[32];
-};
-
-struct LogoutResult : public DataHeader
-{
-	LogoutResult()
-	{
-		dataLength = sizeof(LogoutResult);
-		cmd = CMD_LOGOUT_RESULT;
-		result = 0;
-	}
-	int result;
-};
-
-struct NewUserJoin : public DataHeader
-{
-	NewUserJoin()
-	{
-		dataLength = sizeof(NewUserJoin);
-		cmd = CMD_NEW_USER_JOIN;
-		scok = 0;
-	}
-	int scok;
-};
-
-int processor(SOCKET _cSock)
-{
-	//缓冲区
-	char szRecv[4096] = {};
-	// 5 接收客户端数据
-	int nLen = recv(_cSock, szRecv, sizeof(DataHeader), 0);
-	DataHeader* header = (DataHeader*)szRecv;
-	if (nLen <= 0)
-	{
-		printf("与服务器断开连接，任务结束。\n", _cSock);
-		return -1;
-	}
-	switch (header->cmd)
-	{
-		case CMD_LOGIN_RESULT:
-		{
-			recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
-			LoginResult* login = (LoginResult*)szRecv;
-			printf("收到服务端消息：CMD_LOGIN_RESULT,数据长度：%d\n", login->dataLength);
-		}
-		break;
-		case CMD_LOGOUT_RESULT:
-		{
-			recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
-			LogoutResult* logout = (LogoutResult*)szRecv;
-			printf("收到服务端消息：CMD_LOGOUT_RESULT,数据长度：%d\n", logout->dataLength);
-		}
-		break;
-		case CMD_NEW_USER_JOIN:
-		{
-			recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
-			NewUserJoin* userJoin = (NewUserJoin*)szRecv;
-			printf("收到服务端消息：CMD_NEW_USER_JOIN,数据长度：%d\n", _cSock, userJoin->dataLength);
-		}
-		break;
-	}
-}
-bool g_bRun = true;
-void cmdThread(SOCKET sock)
+void cmdThread(EasyTcpClient* client)
 {
 	while (true)
 	{
@@ -124,7 +9,7 @@ void cmdThread(SOCKET sock)
 		scanf("%s", cmdBuf);
 		if (0 == strcmp(cmdBuf, "exit"))
 		{
-			g_bRun = false;
+			client->Close();
 			printf("退出cmdThread线程\n");
 			break;
 		}
@@ -133,13 +18,14 @@ void cmdThread(SOCKET sock)
 			Login login;
 			strcpy(login.userName, "lyd");
 			strcpy(login.PassWord, "lydmm");
-			send(sock, (const char*)&login, sizeof(Login), 0);
+			client->SendData(&login);
+
 		}
 		else if (0 == strcmp(cmdBuf, "logout"))
 		{
 			Logout logout;
 			strcpy(logout.userName, "lyd");
-			send(sock, (const char*)&logout, sizeof(Logout), 0);
+			client->SendData(&logout);
 		}
 		else {
 			printf("不支持的命令。\n");
@@ -149,69 +35,27 @@ void cmdThread(SOCKET sock)
 
 int main()
 {
-	//启动Windows socket 2.x环境
-	WORD ver = MAKEWORD(2, 2);
-	WSADATA dat;
-	WSAStartup(ver, &dat);
-	//------------
-	//-- 用Socket API建立简易TCP客户端
-	// 1 建立一个socket
-	SOCKET _sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (INVALID_SOCKET == _sock)
+	EasyTcpClient client1;
+	client1.Connect("127.0.0.1", 4567);
+
+	//启动UI线程
+	//新产生的线程会调用 fn 函数，该函数的参数由 args 给出。 thread(Fn&& fn, Args&&... args);
+	std::thread t1(cmdThread, &client1);
+	t1.detach();
+
+	Login login;
+	//char *strcpy(char* dest, const char *src) 把src的复制到dest之下
+	strcpy(login.userName, "lyd");
+	strcpy(login.PassWord, "lydmm");
+	while (client1.isRun())
 	{
-		printf("错误，建立Socket失败...\n");
+		client1.OnRun();
+		client1.SendData(&login);
+		//printf("空闲时间处理其它业务..\n");
+		//Sleep(1000);
 	}
-	else {
-		printf("建立Socket成功...\n");
-	}
-	// 2 连接服务器 connect
-	sockaddr_in _sin = {};
-	_sin.sin_family = AF_INET;
-	_sin.sin_port = htons(4567);
-	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-	int ret  = connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in));
-	if (SOCKET_ERROR == ret)
-	{
-		printf("错误，连接服务器失败...\n");
-	}
-	else {
-		printf("连接服务器成功...\n");
-	}
-	//启动线程
-	std::thread t1(cmdThread,_sock);
-	t1.detach();//线程为分离状态
+	client1.Close();
 
-	while (g_bRun)
-	{
-		fd_set fdReads;
-		FD_ZERO(&fdReads);
-		FD_SET(_sock, &fdReads);
-		timeval t = {1,0};//1s
-		int ret = select(_sock, &fdReads, 0, 0, &t);//1s之内如果没有检测到fdReads函数那么就返回 服务器是非阻塞模型
-		if (ret < 0)
-		{
-			printf("select任务结束1\n");
-			break;
-		}
-		if (FD_ISSET(_sock, &fdReads))//判断标志位
-		{
-			FD_CLR(_sock, &fdReads);//标志位清0
-
-			if (-1 == processor(_sock))
-			{
-				printf("select任务结束2\n");
-				break;
-			}
-		}
-
-		printf("空闲时间处理其它业务..\n");
-
-		Sleep(1000);
-	}
-	// 7 关闭套节字closesocket
-	closesocket(_sock);
-	//清除Windows socket环境
-	WSACleanup();
 	printf("已退出。\n");
 	getchar();
 	return 0;
